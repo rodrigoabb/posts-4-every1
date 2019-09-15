ccd590bae9102ab63e6b23b54db1b42719c09e84
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-15 18:28:27 -0300

    Added SignIn and SingUp components and its routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import PostDetails from './components/posts/PostDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 import 'materialize-css'; // It installs the JS asset only
 import 'materialize-css/dist/css/materialize.min.css';
@@ -15,6 +17,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={ Dashboard } />
           <Route path='/posts/:id' component= {PostDetails} />
+          <Route path='/signin' component= { SignIn } />
+          <Route path='/signup' component= { SignUp }/>
         </Switch>
       </div>
     </BrowserRouter>
