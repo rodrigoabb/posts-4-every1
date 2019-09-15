37d6c96822a493564052a8128f72f76a891d4526
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-15 18:45:45 -0300

    Fixed className instead of class

--- src/components/posts/PostSummary.js
+++ src/components/posts/PostSummary.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PostSummary = () => {
   return (
-    <div class="card z-depth-0 project-summary">
+    <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title">Project Title</span>
           <p>Posted by Rodri</p>
