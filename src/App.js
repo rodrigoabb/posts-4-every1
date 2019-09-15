bbc51921ed46136cc6b1dcce20a71e239bfac0d6
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-14 23:54:24 -0300

    Added initial structure for App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import 'materialize-css'; // It installs the JS asset only
 import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Post 4 Every1</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Post 4 Every1</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
