9d65f426217a48662090147c66b0c2aabe705b15
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-15 02:22:56 -0300

    Added Dashboard component with mock data, post list and notification components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+
 import 'materialize-css'; // It installs the JS asset only
 import 'materialize-css/dist/css/materialize.min.css';
 
@@ -9,6 +11,9 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path='/' component={ Dashboard } />
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/posts/PostList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const PostList = () => {
+  return (
+    <div className="project-list section">
+
+      <div class="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Rodri</p>
+          <p className="grey-text"> 3erd September, 2am</p>
+        </div>
+      </div>
+
+      <div class="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Rodri</p>
+          <p className="grey-text"> 3erd September, 2am</p>
+        </div>
+      </div>
+
+      <div class="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Rodri</p>
+          <p className="grey-text"> 3erd September, 2am</p>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default PostList;
