0ffcbcbbe5deea57fadc2d738d943f389f4a77c4
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-15 02:28:44 -0300

    Implemented PostSummary component

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,33 +1,13 @@
 import React from 'react';
+import PostSummary from './PostSummary';
 
 const PostList = () => {
   return (
     <div className="project-list section">
-
-      <div class="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Rodri</p>
-          <p className="grey-text"> 3erd September, 2am</p>
-        </div>
-      </div>
-
-      <div class="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Rodri</p>
-          <p className="grey-text"> 3erd September, 2am</p>
-        </div>
-      </div>
-
-      <div class="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Rodri</p>
-          <p className="grey-text"> 3erd September, 2am</p>
-        </div>
-      </div>
-
+      <PostSummary />
+      <PostSummary />
+      <PostSummary />
+      <PostSummary />
 
     </div>
   )
---
+++ src/components/posts/PostSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PostSummary = () => {
+  return (
+    <div class="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Rodri</p>
+          <p className="grey-text"> 3erd September, 2am</p>
+        </div>
+      </div>
+  )
+}
+
+export default PostSummary;
