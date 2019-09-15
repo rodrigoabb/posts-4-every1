a5d1a082b441318e13bc052b32d9588984db1a87
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-14 23:44:36 -0300

    Added materialize css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Post 4 Every1</h1>
     </div>
   );
 }
