60c2ed9e2ceb699e1e6f779bcd963d183d5737e9
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-09-21 18:22:16 -0300

    Added store enhancers

--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,6 +1,6 @@
 // With thunk we return a function - We pause the dispatch
 export const createPost = (post) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make asynch call to db
     dispatch({ type: 'CREATE_POST', post });
   }
