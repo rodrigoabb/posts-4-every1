4443528f0c428211134807becb88a595494ecae3
Rodrigo Abbás <Rodrigoabbas@gmail.com>
2019-10-06 19:39:58 -0300

    Made users able to log in

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,9 +1,24 @@
 const initState = {
-
+  authError: null
 };
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch(action.type) {
+    case 'LOGIN_ERROR' :
+      console.log('Login Failed');
+      return {
+        ...state,
+        authError: 'Login failed'
+      }
+    case 'LOGIN_SUCCESS' :
+      console.log('Login Success');
+      return {
+        ...state,
+        authError: null
+      }
+    default :
+      return state;
+  }
 }
 
 export default authReducer;
